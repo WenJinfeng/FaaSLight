054ea359b9051ac9ee9de2d66ad2ebb670b5cec4
JinfengCode <79156929+JinfengCode@users.noreply.github.com>
2022-09-19 13:19:47 +0800

    Update integrationFunMain.py

--- integrationFunMain.py
+++ integrationFunMain.py
@@ -1,6 +1,9 @@
 
 import os
 import json
+
+import sys
+sys.setrecursionlimit(5000000)
 import shutil
 
 import time
@@ -16,17 +19,17 @@ assetsDir = {
     # ignore
     "ignorDir" : ["__pycache__", "tests", ".serverless", "pip", "pkg_resources", "setuptools", "wheel", "_distutils_hack"], 
 
-    "ignorSpeDir": [],  
+    "ignorSpeDir": [".dist-info"],  
     
     "ignorFile": [".pyc", ".pyi", ".pth", ".virtualenv","_virtualenv.py",".md"],
 }
 
-
+# 1
 def prepare():
     import removefile
     removefile.delFiles(input_package, assetsDir)
 
-
+# 2
 def serverless_func():
     entry_py = ["handler"]
     input_entry_point =[]
@@ -46,63 +49,6 @@ def magic_func():
     os.system("python3 find_magic_origin.py --dirname {} --path {} --moshuoutput {}".format(dir_name, input_package, moshu_file))
     # Meanwhile, find serverless functions and save into entry_point.txt to service the component construct_graph()
     return Identify_name,moshu_file
-
-
-def special_rule():
-    global Identify_name,moshu_file, input_package
-    #write the used package in used_package_name
-    used_package_name = "used_func_result/used_package_{}.txt".format(Identify_name)
-
-    # find magic functions related to used packages
-    moshu_file_final = "moshu_functions/{}_final.txt".format(Identify_name)
-    os.system("python3 find_magic.py --dirname {} --path {} --packageset {} --moshuoutput {}".format(dir_name, input_package, used_package_name, moshu_file_final))
-
-    # optinal: start - pre-loaded function generation for any package
-
-    # input_package_dynamic_copy = "{}-dynamic".format(input_package)
-    # shutil.copytree(input_package,input_package_dynamic_copy)
-
-    # os.system("python3 importprocess.py --path {} ".format(input_package_dynamic_copy))
-
-    # find_package_name = "sklearn"
-    # log_name = "log-{}.log".format(find_package_name)
-    # execute_py = "lambda_function.py"
-    # os.system("python3 {}/{} >log/{}".format(input_package_dynamic_copy,execute_py,log_name))
-
-    # log_genrate = "log/{}".format(log_name)
-    # log_handle = "import-prefunc/{}-python36.txt".format(find_package_name)
-
-    # import importsort
-    # importsort.sortresult(log_genrate, input_package_dynamic_copy, log_handle)
-
-    # shutil.rmtree(input_package_dynamic_copy)
-
-    # optinal:  end - pre-loaded function generation
-
-
-    used_fun_result_output_final_re = "used_func_result/used_func_{}_final_re.txt".format(Identify_name)
-    prefunc_dir = "import-prefunc"
-
-    import processUtil
-    processUtil.result_addlibray([], used_package_name, prefunc_dir, used_fun_result_output_final_re)
-
-
-
-    temp = []
-    if os.path.exists(moshu_file_final):
-        for each_i in open(moshu_file_final):
-            each_i = each_i.strip('\n')
-            temp.append(each_i)
-    if os.path.exists(used_fun_result_output_final_re):
-        for each_i in open(used_fun_result_output_final_re):
-            each_i = each_i.strip('\n')
-            temp.append(each_i)  
-
-
-    input_entry_point = input_entry_point.append(temp)
-
-    return used_package_name,moshu_file_final,input_entry_point
-
 
 def construct_graph():
     global seedfun_list,input_entry_point
@@ -135,7 +81,7 @@ def construct_graph():
         f.close()
 
     import staticAdd
-    staticAdd.add_info(input_package, output_file, input_entry_point, moshu_file_final, re_FunRel)
+    staticAdd.add_info(input_package, output_file, input_entry_point, moshu_file, re_FunRel)
 
     return re_FunRel
 
@@ -146,7 +92,106 @@ def initial_func():
     processUtil.getDynamicContent_new(seedfun_list, input_package, re_FunRel, input_entry_point, used_fun_result_output)
     return used_fun_result_output
 
+def special_rule():
+    global Identify_name,used_fun_result_output,input_package
+    used_package_name = "used_func_result/used_package_{}.txt".format(Identify_name)
 
+    import isContainPackage
+    isContainPackage.getPackgaeName(used_fun_result_output, input_package, used_package_name)
+
+    # find magic functions related to used packages
+    moshu_file_update = "moshu_functions/{}_update.txt".format(Identify_name)
+    os.system("python3 find_magic.py --dirname {} --path {} --packageset {} --moshuoutput {}".format(dir_name, input_package, used_package_name, moshu_file_update))
+
+    # optinal: start - pre-loaded function generation
+
+    # input_package_dynamic_copy = "{}-dynamic".format(input_package)
+    # shutil.copytree(input_package,input_package_dynamic_copy)
+
+    # os.system("python3 importprocess.py --path {} ".format(input_package_dynamic_copy))
+
+    # find_package_name = "sklearn"
+    # log_name = "log-{}.log".format(find_package_name)
+    # execute_py = "lambda_function.py"
+    # os.system("python3 {}/{} >log/{}".format(input_package_dynamic_copy,execute_py,log_name))
+
+    # log_genrate = "log/{}".format(log_name)
+    # log_handle = "import-prefunc/{}-python36.txt".format(find_package_name)
+
+    # import importsort
+    # importsort.sortresult(log_genrate, input_package_dynamic_copy, log_handle)
+
+    # shutil.rmtree(input_package_dynamic_copy)
+
+    # optinal:  end - pre-loaded function generation
+
+
+
+    # whitelist
+    special_key=["requests.utils.set_environ", 
+    "requests.sessions.Session.request", 
+    "requests.adapters.HTTPAdapter.send", 
+    "urllib3.connectionpool.connection_from_url",
+    "numpy.linalg.lapack_lite", 
+    "numpy.lib._iotools.NameValidator",
+    "numpy.lib._iotools._decode_line",
+    "numpy.lib._iotools.easy_dtype",
+    "numpy.lib.npyio.loadtxt",
+    "numpy.lib.npyio.loadtxt.flatten_dtype_internal",
+    "numpy.lib.npyio.loadtxt.read_data",
+    "numpy.lib.npyio.loadtxt.pack_items",
+    "numpy.lib.npyio.loadtxt.split_line",
+    "mpl_toolkits.axes_grid1.parasite_axes.host_axes_class_factory",
+    "sklearn.ensemble._forest._parallel_build_trees",
+    "pandas.io.parsers._read",
+    "pandas.io.excel._xlwt._XlwtWriter",
+    "pandas.io.excel._xlsxwriter._XlsxWriter",
+    "pandas.io.excel._openpyxl._OpenpyxlWriter",
+    "pandas.io.excel._odswriter._ODSWriter",
+    "pandas.io.excel._base.ExcelWriter",
+    "tensorflow.python.platform.resource_loader.get_path_to_datafile",
+    "tensorflow.python.training.saving.saveable_object_util.saveable_objects_for_op",
+    "tensorflow.python.keras.saving.saved_model.serialized_attributes.CommonEndpoints",
+    "tensorflow.python.training.saver.BaseSaverBuilder",
+    "tensorflow.python.summary.writer.writer.FileWriter",
+    "tensorflow.python.keras.engine.base_layer.Layer",
+    "urllib3.request.RequestMethods",
+    "keras.saving.saved_model.load_context.load_context",
+    "absl.third_party.unittest3_backport.result.TextTestResult",
+    "keras.optimizer_v1.Optimizer"
+    ]
+    moshu_file_final = "moshu_functions/{}_final.txt".format(Identify_name)
+
+    import processUtil
+    processUtil.moshu_update(moshu_file_update, special_key, moshu_file_final)
+
+    global seedfun_list,re_FunRel,input_entry_point
+
+    
+    used_fun_result_output_update = "used_func_result/used_func_{}_update.txt".format(Identify_name)
+    processUtil.getDynamicContent(seedfun_list, input_package, re_FunRel, input_entry_point, moshu_file_final, used_fun_result_output_update)
+    used_fun_result_output_final = "used_func_result/used_func_{}_final.txt".format(Identify_name)
+
+    special_key_append = ["unittest.TestCase"]
+    special_key_append = []
+    processUtil.result_process(input_package, used_fun_result_output_update,used_fun_result_output_final, special_key_append)
+
+    
+    return used_package_name,moshu_file_final,used_fun_result_output_final
+
+# def final_rule():
+#     global Identify_name,seedfun_list,input_package,re_FunRel,input_entry_point,moshu_file_final
+
+#     import processUtil
+#     used_fun_result_output_update = "used_func_result/used_func_{}_update.txt".format(Identify_name)
+#     processUtil.getDynamicContent(seedfun_list, input_package, re_FunRel, input_entry_point, moshu_file_final, used_fun_result_output_update)
+#     used_fun_result_output_final = "used_func_result/used_func_{}_final.txt".format(Identify_name)
+
+#     special_key_append = ["unittest.TestCase"]
+#     special_key_append = []
+#     processUtil.result_process(input_package, used_fun_result_output_update,used_fun_result_output_final, special_key_append)
+
+#     return used_fun_result_output_final
 
 def func_rewrite():
     global Identify_name,used_fun_result_output_final,used_package_name
@@ -170,31 +215,76 @@ def func_rewrite():
     
     return used_fun_result_output_final_re,buits_list_file
 
+# def import_rewrite():
+#     global used_fun_result_output_final_re, buits_list_file
+
+#     input_package_copy = input_package
+
+#     import removefile
+#     removefile.delFiles(input_package_copy, assetsDir)
+
+#     module_name = "module_file/module_name_{}.txt".format(Identify_name)
+
+#     import utiltool
+#     utiltool.get_module(input_package_copy, module_name)
+
+#     os.system("python3 Import_rewriting.py --dirname {} --path {} --usedfuntionlist {} --builtlist {} --module_name {}".format(dir_name, input_package_copy, used_fun_result_output_final_re, buits_list_file, module_name))
+
+#     shutil.copytree("lazy_import","{}/lazy_import".format(input_package_copy))
+#     shutil.copy("six.py", "{}/six.py".format(input_package_copy))
+
+
 if __name__ == "__main__": 
     
-    # Step1: Optional File Elimination
+    # Step1: Preprocessing
     print("step1 start")
     prepare()
     print("step1 end")
 
-    # Step2: Serverless Function Recognition
+    # Step2: Serverless function
     print("step2 start")
     ymlFile,seedfun_list,entry_py,input_entry_point = serverless_func()
     print("step2 end")
 
-    # Step3: Special Function Recognition
+    # Step3: Magic function
     print("step3 start")
     Identify_name,moshu_file = magic_func()
-    used_package_name,moshu_file_final,input_entry_point = special_rule()
     print("step3 end")
 
-    # Step4: Optional Function Generation 
+    time_tmp = time.time()
+
+    # Step4: Constructing call graph 
     print("step4 start")
     re_FunRel = construct_graph()
-    used_fun_result_output = initial_func()
     print("step4 end")
 
-    # Step5: Function-level Rewriting
+    time_end = time.time()
+    print("Spending of step4:"+str(time_end-time_tmp))
+
+    # Step5: Initial useful function generation
     print("step5 start")
-    used_fun_result_output_final_re,buits_list_file = func_rewrite()
+    used_fun_result_output = initial_func()
     print("step5 end")
+
+    # Step6: Special rule query
+    print("step6 start")
+    used_package_name,moshu_file_final,used_fun_result_output_final = special_rule()
+    print("step6 end")
+
+    # # Step7: Final useful function generation
+    # print("step7 start")
+    # used_fun_result_output_final = final_rule()
+    # print("step7 end")
+
+    # Step8: Function-level rewriting
+    print("step8 start")
+    used_fun_result_output_final_re,buits_list_file = func_rewrite()
+    print("step8 end")
+
+    # # Step9: Import-level rewriting
+    # print("step9 start")
+    # import_rewrite()
+    # print("step9 end")
+
+    time_tmp = time.time()
+    print("All spending:"+str(time_tmp-time_start))
